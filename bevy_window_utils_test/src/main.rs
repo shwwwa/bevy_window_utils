499c062567f5db6675c2f17f1d9d1858f322fde6
caffidev <caffidev@gmail.com>
2025-04-23 19:38:44 +1000

    docs: docs

--- bevy_window_utils_test/src/main.rs
+++ bevy_window_utils_test/src/main.rs
@@ -3,10 +3,13 @@ use bevy_window_utils::{WindowUtils, WindowUtilsPlugin};
 
 fn main() {
     let mut app = App::new();
-    app.add_plugins((WindowUtilsPlugin, DefaultPlugins))
+
+    // You can initialize icon instead of default.
+    app.add_plugins((DefaultPlugins, WindowUtilsPlugin::default()))
         .add_systems(
             Startup,
             |assets: Res<AssetServer>, mut window: ResMut<WindowUtils>| {
+		// You can set icon on runtime:
                 window.window_icon = Some(assets.load("icon.png"));
                 window.taskbar_progress = Some(bevy_window_utils::TaskbarProgress {
                     progress: 30,
