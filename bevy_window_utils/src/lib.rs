dbdb87802441b9f49ae14228eafcb6e8ca8a852b
caffidev <caffidev@gmail.com>
2025-04-23 18:47:58 +1000

    feat: is_maximized(); states in taskbar progress

--- bevy_window_utils/src/lib.rs
+++ bevy_window_utils/src/lib.rs
@@ -1,54 +1,63 @@
+#![allow(clippy::pedantic)]
+
 #[allow(unused_imports)]
 use std::os::raw::c_void;
 
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy::winit::WinitWindows;
+use bevy::{app::Plugin, prelude::Resource};
 #[cfg(target_os = "windows")]
 use winit::raw_window_handle;
 #[cfg(target_os = "windows")]
 use winit::raw_window_handle::HasWindowHandle;
-use bevy::prelude::*;
-use bevy::winit::WinitWindows;
-use bevy::{app::Plugin, prelude::Resource};
 
 #[cfg(target_os = "windows")]
-use w::prelude::{shell_ITaskbarList3, Handle};
+use w::prelude::{Handle, shell_ITaskbarList3};
 #[cfg(target_os = "windows")]
-use w::{ITaskbarList4, HWND};
+use w::{HWND, ITaskbarList4};
 #[cfg(target_os = "windows")]
 use winsafe::{self as w, co};
 
 /** A [`Plugin`] that defines an interface for extended windowing support in Bevy.
 
-    Adds barely exposed things to bevy like setting window icons, taskbar progress, or other winit/winsafe options. */
+Adds barely exposed things to bevy like setting window icons, taskbar progress, or other winit/winsafe options. */
 pub struct WindowUtilsPlugin;
 
 impl Plugin for WindowUtilsPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<WindowUtils>()
-            .add_systems(Update, window_utils_resource_updated);
+            .add_systems(Update, window_utils_resource_updated)
+            .add_systems(Update, update_is_maximized);
     }
 }
 
 #[cfg(feature = "taskbar")]
-/** Struct for taskbar progress. Supports only windows 7+. Requires `taskbar` feature.
-    Provides useful interface from COM:
-    https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nn-shobjidl_core-itaskbarlist3
- */
+/** Struct for taskbar progress. Requires `taskbar` feature.
+   Provides useful interface from COM:
+   https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nn-shobjidl_core-itaskbarlist3
+
+   Supports:
+   - Windows (7+)
+*/
 pub struct TaskbarProgress {
     /** Indicates the proportion of the operation that has been completed. */
     pub progress: u64,
     /** Indicates the value progress will have when the operation is complete. */
     pub max: u64,
     /// Indicates the type and state of the progress indicator displayed on a taskbar button.
-    /// Note that a call to SetProgressValue will switch a progress indicator
+    /// Note that a call to SetProgressValue should switch a progress indicator
     /// currently in an indeterminate mode (TBPF_INDETERMINATE) to a normal (determinate) display
-    /// and clear the TBPF_INDETERMINATE flag.
+    /// and clear the TBPF_INDETERMINATE flag, but then is overwritten with state change, so it needs manual change.
     pub state: TaskbarState,
+    /// Automatically stops the progress when [`TaskbarProgress::progress`] exceeds [`TaskbarProgress::max`].
+    pub auto_no_progress: bool,
 }
 
 /// Sets the type and state of the progress indicator displayed on a taskbar button.
 /// Note that a call to SetProgressValue should switch a progress indicator
 /// currently in an indeterminate mode (TBPF_INDETERMINATE) to a normal (determinate) display
-/// and clear the TBPF_INDETERMINATE flag, but is overwritten with state change, so change it manually.
+/// and clear the TBPF_INDETERMINATE flag, but is overwritten with state change, so it needs manual change.
 #[derive(Copy, Clone)]
 pub enum TaskbarState {
     /// Stops displaying progress and returns the button to its normal state.
@@ -81,12 +90,6 @@ pub enum TaskbarState {
     Paused = 0x8,
 }
 
-impl From<isize> for TaskbarState {
-    fn from(value: isize) -> Self {
-	value.into()
-    }
-}
-
 /** Main resource with access to additional exposed things from winit. */
 #[derive(Resource, Default)]
 pub struct WindowUtils {
@@ -94,10 +97,44 @@ pub struct WindowUtils {
     /** Current taskbar progress. Supports only windows 7+. Requires `taskbar` feature.
     Provides useful interface from COM:
     https://learn.microsoft.com/en-us/windows/win32/api/shobjidl_core/nn-shobjidl_core-itaskbarlist3
+
+    Supports:
+    - Windows (7+)
     */
     pub taskbar_progress: Option<TaskbarProgress>,
-    /** Contains handle to window's icon. If resource is invalid throws [`bevy_asset::server`] error to console. */
+    /** Contains handle to window's icon. If resource is invalid throws `bevy_asset::server` error to console.
+    Supports:
+    - Windows
+    - Linux
+    - MacOS (?)
+     */
     pub window_icon: Option<bevy::asset::Handle<Image>>,
+    /** Automatically changes its value whether window is maximized or not. Returns None if error happened.
+    Requires existence of primary window.
+    Supports:
+    - Windows
+    - Linux
+    - MacOS (?)
+     */
+    pub is_maximized: Option<bool>,
+}
+
+fn update_is_maximized(
+    mut window_utils: ResMut<WindowUtils>,
+    windows: NonSend<WinitWindows>,
+    window: Query<EntityRef, With<PrimaryWindow>>,
+) {
+    for entity in window.iter() {
+        match windows.get_window(entity.id()) {
+            Some(window_wrapper) => {
+                window_utils.is_maximized = Some(window_wrapper.is_maximized());
+            }
+            None => {
+                warn_once!("winit is_maximized() interception failed, couldn't get the window.");
+                window_utils.is_maximized = None;
+            }
+        }
+    }
 }
 
 fn window_utils_resource_updated(
@@ -105,7 +142,6 @@ fn window_utils_resource_updated(
     windows: NonSend<WinitWindows>,
     assets: Res<Assets<Image>>,
 ) {
-    // If something from this crate was changed.
     if assets.is_changed() || window_utils.is_changed() {
         let icon = window_utils
             .window_icon
@@ -113,19 +149,19 @@ fn window_utils_resource_updated(
             .and_then(|i| assets.get(i))
             .and_then(|i| {
                 ::winit::window::Icon::from_rgba(
-		    // safe unwrap because it is not being used as a storage texture.
+                    // safe unwrap because it is not being used as a storage texture.
                     i.data.clone().unwrap(),
                     i.texture_descriptor.size.width,
                     i.texture_descriptor.size.height,
                 )
                 .ok()
             });
-	
+
         for window in windows.windows.iter() {
             window.1.set_window_icon(icon.clone())
         }
-	
-        // Taskbar currently only supports windows.
+
+        // Taskbar: supports only windows
         #[cfg(all(feature = "taskbar", target_os = "windows"))]
         if window_utils.is_changed() {
             {
@@ -139,20 +175,40 @@ fn window_utils_resource_updated(
                         .unwrap();
                         // unsafe: winit holds HWND as an NonZeroIsize while winsafe uses a pointer.
                         // requires rwh_06 feature (gets raw_window_handle v0.6) from winit that is provided by default.
-			unsafe {
-			    match window.1.window_handle() {
-				Ok(handle) => {
-				    // We know for sure that enum is Win32, so access hwnd directly
-				    if let raw_window_handle::RawWindowHandle::Win32(win_handle) = handle.as_raw() {
-					let hwnd = HWND::from_ptr(isize::from(win_handle.hwnd) as *mut c_void);
-					itbl.SetProgressValue(&hwnd, progress.progress, progress.max).unwrap();
-					itbl.SetProgressState(&hwnd, co::TBPF::from_raw(progress.state as u32)).unwrap();
-				    }
-				},
-				Err(e) => {
-				    warn!("Couldn't set taskbar progress: {}", e);
-				}
-			    }
+                        unsafe {
+                            match window.1.window_handle() {
+                                Ok(handle) => {
+                                    // We know for sure that enum is Win32, so access hwnd directly
+                                    if let raw_window_handle::RawWindowHandle::Win32(win_handle) =
+                                        handle.as_raw()
+                                    {
+                                        let hwnd = HWND::from_ptr(
+                                            isize::from(win_handle.hwnd) as *mut c_void
+                                        );
+                                        itbl.SetProgressValue(
+                                            &hwnd,
+                                            progress.progress,
+                                            progress.max,
+                                        )
+                                        .unwrap();
+                                        if progress.auto_no_progress
+                                            && progress.progress >= progress.max
+                                        {
+                                            itbl.SetProgressState(&hwnd, co::TBPF::NOPROGRESS)
+                                                .unwrap();
+                                        } else {
+                                            itbl.SetProgressState(
+                                                &hwnd,
+                                                co::TBPF::from_raw(progress.state as u32),
+                                            )
+                                            .unwrap();
+                                        }
+                                    }
+                                }
+                                Err(e) => {
+                                    warn!("Couldn't set taskbar progress: {}", e);
+                                }
+                            }
                         }
                     }
                 }
